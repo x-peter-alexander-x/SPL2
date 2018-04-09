ae748268dd6cfccf2c046c5e4a38fdb4b8f2b915
x-peter-alexander-x <171wita04@o365.htl-leoben.at>
2018-04-09 14:56:36 +0200

    Erweiterung des Programmes

--- bus-und-haltestelle.py
+++ bus-und-haltestelle.py
@@ -4,10 +4,14 @@ i = input("Wie viele Haltestellen")
 for x in range (0, int(i)):
     z = input("Wie viele steigen ein?")
     a = input("Wie viele steigen aus?")
+    if(p<int(a),(int(a)>int(z))):
+        z=0
+        a=0
+        int(i)+1
     p = p - int(a)
     p = p + int(z)
-    if(p<60, p<=0):
-        print("Invalide Anzahl von Personen im Bus")
+    if(p>60):
+        print("Zu hohe Anzahl von Personen im Bus")
         break
     else:
         print("Es sind ",p," Personen im Bus")
